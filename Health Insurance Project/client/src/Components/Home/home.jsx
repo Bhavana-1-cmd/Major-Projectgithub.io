583bea5760a9bbbfe1fed527527f82a7c5a3dbb9
Bhavana-1-cmd <bhavnamishra.jbp@gmail.com>
2023-04-11 22:04:00 +0530

    Initial commit

---
+++ Health Insurance Project/client/src/Components/Home/home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./home.css"
+import { Link } from 'react-router-dom'
+import Auth from '../../Pages/Auth/Auth'
+import contact from '../../Pages/contact/contact'
+
+
+
+const home = () => {
+  return (
+   
+    <div className='d-flex home-container' >
+      <div className="joinUsContainer d-flex flex-column justify-content-center" style={{width:"50%",padding:"10%"}}>
+      <h1 style={{textColor:"dark blue"}}>Web<br/>Page<br/></h1>
+      <p style={{color:"white"}}>Here our team is always with you in your bad days....</p>
+      <button type='button' className='btn home-btn'><Link to="/Insurance" style={{textDecoration:"none"}}>Join Us</Link></button>
+      </div>
+      <Auth/>
+      
+    </div>
+  )
+}
+
+export default home
---
+++ Health Insurance Project/client/src/Pages/services/services.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+
+
+const Services = () => {
+    return (
+        <>
+    <section className="services-main-container">
+
+
+           <div className="container service-container">
+   <h1 className="main-heading text-center font-weight-bold">
+    Our service
+    </h1>
+    <div className="row">
+        <div className="col-11 col-lg-3 col-xxl-4 work-container-subdiv">
+            <i className="fontawesome-style"></i>
+            <h2 className="sub-heading">Claim processing service</h2>
+            <p>
+                i dont know
+            </p>
+
+</div>
+                    <div class="service">
+                        <i class="fa-solid fa-house-medical-circle-check"></i>
+                        <h2>claim support</h2> 
+                         <p>i dont know.</p>   
+            </div>
+                    <div class="service">
+                        <i class="fa-solid fa-hand-holding-dollar"></i>
+                        <h2>Loss assesor</h2> 
+                         <p>i dont know.</p>   
+            </div>
+                    <div class="service">
+                        <i class="fa-regular fa-handshake"></i>
+                        <h2>Claim surveyor service</h2> 
+                         <p>i dont know.</p>   
+            </div>
+                    <div class="service">
+                        <i class="fa-regular fa-heart"></i>
+                        <h2>Claim valuation</h2> 
+                         <p>i dont know.</p>   
+            </div>
+                    <div class="service">
+                        <i class="fa-solid fa-user-secret"></i>
+                        <h2>Claim investigation</h2> 
+                         <p>i dont know.</p>   
+            </div>
+                    <div class="service">
+                       <i class="fa-solid fa-mobile"></i>
+                       <h2>Cashless Service</h2> 
+                         <p>i dont know.</p>   
+    </div>
+       </div>
+           </div>
+ </section>
+ </>
+       
+    );
+};
+
+export default Services;
